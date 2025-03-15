8624565f1c8bd0fe72bb7017ea016635167868e8
khalid-fannas <kha2000.khaled@gmail.com>
2025-03-15 15:37:29 +0300

    Confirm password Added

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,15 @@ const jwt = require("jsonwebtoken");
 const db = require("../config/db");
 
 exports.register = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, confirmPassword } = req.body;
 
-  if (!username || !email || !password) {
+  if (!username || !email || !password || confirmPassword) {
     return res.status(400).json({ Message: "Please fill in all fields." });
   }
 
+  if (password !== confirmPassword) {
+    return res.status(400).json({ Message: "Password do not match" });
+  }
   try {
     const [existingUser] = await db.query(
       "SELECT * FROM users WHERE email = ?",
